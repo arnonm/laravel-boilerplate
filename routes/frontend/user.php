12e7e462f92bc6a67e811d82e0cecda33e0b16ec
Arnon Meshoulam <arnonm@gmail.com>
2020-08-27 17:10:40 +0300

    Addition of profile routes

--- routes/frontend/user.php
+++ routes/frontend/user.php
@@ -2,7 +2,11 @@
 
 use App\Http\Controllers\Frontend\User\AccountController;
 use App\Http\Controllers\Frontend\User\DashboardController;
+use App\Http\Controllers\Frontend\User\ProfileContactController;
 use App\Http\Controllers\Frontend\User\ProfileController;
+use App\Http\Controllers\Frontend\User\ProfileEmergencyController;
+use App\Http\Controllers\Frontend\User\ProfileLicenseController;
+use App\Http\Controllers\Frontend\User\ProfileUniformController;
 use Tabuna\Breadcrumbs\Trail;
 
 /*
@@ -16,15 +20,50 @@ Route::group(['as' => 'user.', 'middleware' => ['auth', 'password.expires', conf
         ->name('dashboard')
         ->breadcrumbs(function (Trail $trail) {
             $trail->parent('frontend.index')
-                ->push(__('Dashboard'), route('frontend.user.dashboard'));
+                ->push(__('global.Dashboard'), route('frontend.user.dashboard'));
         });
 
     Route::get('account', [AccountController::class, 'index'])
         ->name('account')
         ->breadcrumbs(function (Trail $trail) {
             $trail->parent('frontend.index')
-                ->push(__('My Account'), route('frontend.user.account'));
+                ->push(__('global.My Account'), route('frontend.user.account'));
         });
 
+
+    Route::get('profile/contact/edit', [ProfileContactController::class, 'edit'])
+        ->name('profile.contact.edit');
+
+    Route::get('profile/information/edit', function () {
+        return view('frontend.user.account.edit.information');
+    })->name('profile.information.edit');
+
+    Route::get('profile/vehicle/edit', function () {
+        return view('frontend.user.account.edit.vehicle');
+    })->name('profile.vehicle.edit');
+
+    Route::get('profile/license/edit', [ProfileLicenseController::class, 'edit'])
+        ->name('profile.license.edit');
+
+    Route::get('profile/emergency/edit', [ProfileEmergencyController::class, 'edit'])
+        ->name('profile.emergency.edit');
+
+    Route::get('profile/uniform/edit', [ProfileUniformController::class, 'edit'])
+        ->name('profile.uniform.edit');
+
     Route::patch('profile/update', [ProfileController::class, 'update'])->name('profile.update');
+
+
+    Route::patch('profile/contact/update', [ProfileContactController::class, 'update'])
+        ->name('profile.contact.update');
+
+    Route::patch('profile/emergency/update', [ProfileContactController::class, 'update'])
+        ->name('profile.emergency.update');
+
+    Route::patch('profile/contact/update', [ProfileLicenseController::class, 'update'])
+        ->name('profile.license.update');
+
+    Route::patch('profile/uniform/update', [ProfileUniformController::class, 'update'])
+        ->name('profile.uniform.update');
+
 });
