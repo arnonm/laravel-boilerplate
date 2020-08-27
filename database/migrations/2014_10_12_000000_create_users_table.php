4f4bf36c11a3570a57f5633a4ff48bb616aed9c1
Arnon Meshoulam <arnonm@gmail.com>
2020-08-27 17:41:06 +0300

    change bigIncrements to increments for id

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,7 +15,7 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->bigIncrements('id');
+            $table->increments('id');
             $table->enum('type', [User::TYPE_ADMIN, User::TYPE_USER])->default(User::TYPE_USER);
             $table->string('name');
             $table->string('email')->unique()->nullable();
