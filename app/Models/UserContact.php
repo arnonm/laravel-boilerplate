f1736be2cb7961f2acd1af53bd511b6b283dea9e
Arnon Meshoulam <arnonm@gmail.com>
2020-09-09 21:29:47 +0300

    Added User depedency

--- app/Models/UserContact.php
+++ app/Models/UserContact.php
@@ -2,11 +2,13 @@
 
 namespace App\Models;
 
+use App\Domains\Auth\Models\User;
 use Illuminate\Database\Eloquent\Model;
 
 class UserContact extends Model
 {
     protected $fillable = ['name', 'relation', 'national_id', 'phone', 'user_id'];
+
     public function user()
     {
         return $this->belongsTo(User::class);
